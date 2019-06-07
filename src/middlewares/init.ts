083dfff72f58a6a49e60440da9c422a18ef2a03b
Ardalan Amini <ardalanamini22@gmail.com>
2019-06-07 07:14:11 +0430

    WIP

--- src/middlewares/init.ts
+++ src/middlewares/init.ts
@@ -1,14 +1,10 @@
 import Foxify from "..";
-import { name } from "../../package.json";
-import * as utils from "../utils";
 
 const init = (app: Foxify): Foxify.Handler | null => {
   if (app.disabled("x-powered-by")) return null;
 
-  const xPoweredBy = utils.string.capitalize(name);
-
   return function foxify_init(req, res, next) {
-    res.setHeader("X-Powered-By", xPoweredBy);
+    res.setHeader("X-Powered-By", "Foxify");
 
     next();
   };
