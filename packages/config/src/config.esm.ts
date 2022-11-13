d6cfc8c4acd895cebcb36f30f6556b76e35fe06e
Ardalan Amini <ardalanamini22@gmail.com>
2022-11-13 23:14:21 +0330

    build: Fix Foxify Config CommonJS build issue

--- packages/config/src/config.esm.ts
+++ packages/config/src/config.esm.ts
@@ -2,6 +2,9 @@ import { CONFIG_FILEPATH, ConfigI } from "#src/constants";
 
 /* ------------------------- Read the config file ------------------------- */
 
+// TODO: Just to avoid the CommonJS build issue
+// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error,@typescript-eslint/ban-ts-comment
+// @ts-ignore
 const content: ConfigI = await import(CONFIG_FILEPATH)
   .then(resolved => resolved.default)
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
